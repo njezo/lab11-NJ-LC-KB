608fc2edffa77a12568283ce0e1429fdd1229eac
lydiaazhao <zhao.lydia16@gmail.com>
2026-04-10 19:28:50 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
+# https://github.com/njezo/lab11-NJ-LC-KB/tree/615473fc20e58d2186e0028c1c3ce93342e62975/Lab%20Repository
+# Partner 1: [Your Name]
+# Partner 2: Lydia Zhao
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -20,4 +24,9 @@ def log (a, b):
     return math.log(a, b)
 def exp(a, b):
     return a ** b
-
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+def hypotenuse(a, b):
+    return math.hypot(a, b)
